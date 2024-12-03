16070863d6a7ba3b9931475fa29a6c995927b385
Tobias Addicks <shahondin1624@gmail.com>
2024-12-03 20:16:12 +0100

    Preparation

--- src/etc/mod.rs
+++ src/etc/mod.rs
@@ -1,3 +1,14 @@
 pub mod solution;
 
-pub use solution::Solution;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+pub fn read_input(filename: &str) -> String {
+    let mut path = Path::new("input").join(filename);
+    let mut file = File::open(path).expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    contents
+}
